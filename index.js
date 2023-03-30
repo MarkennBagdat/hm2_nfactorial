090f7814a182272baa90837198c6d3f162a5bd86
MarkennBagdat <129280605+MarkennBagdat@users.noreply.github.com>
2023-03-30 17:03:16 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+let shop = {
+    product: [
+        {
+            name: "Milk",
+            price: "350",
+            mass: "3L",
+            quantity: "45",
+            company: ["ainalayin", "moe"]
+        },
+        {
+            name:"bread",
+            price:"150",
+            mass: "1 piece",
+            quantity:"15",
+            company: ["local", "pesh"]
+        },
+        {
+            name: "Sugar",
+            price:"650",
+            mass:"1kg",
+            quantity:"15kg",
+        }
+    ]}
+
+    console.log(shop.product[0].company[1]);
+
+
+
+/*var vehicle ={
+    brandName: "BMW",
+    model: "X7",
+};
+
+alert(vehicle.model);
+vehicle.model="M8";
+alert(vehicle.model);
+delete(vehicle.model);
+alert(vehicle.model);
+*/
+
+
+/*salary*/
+
+/*let salaries = {
+	Aruzhan: 100000,
+	Baktyar: 160000,
+	Gulzhan: 130000,
+    Nurzhan: 800000,
+    Damelya: 750000,
+}
+
+var sum=0;  
+for (var key in salaries){  
+sum+=salaries[key];  
+}  
+console.log(sum);*/
+
+
